6ac681c9fdd47ace8ae0a70e72a512fb9d6b2a8f
Maksim Lyashenko <maksimlya@gmail.com>
2018-06-13 17:53:29 +0300

    Timer + Leaderboard + Basic animation

--- app/src/main/java/afeka/com/hw1/LeaderboardMap.java
+++ app/src/main/java/afeka/com/hw1/LeaderboardMap.java
@@ -1,5 +1,11 @@
 package afeka.com.hw1;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
@@ -8,13 +14,59 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-public class LeaderboardMap extends SupportMapFragment{
+import android.support.v4.app.FragmentManager;
+
+public class LeaderboardMap extends SupportMapFragment implements OnMapReadyCallback {
     private View view;
     private GoogleMap googleMap;
 
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
+        return super.onCreateView(layoutInflater, viewGroup, bundle);
+    }
 
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        getMapAsync(this);
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        // goToLocation(32.0863863,34.809778, 16);
+
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+
+            }
+    }
+        googleMap.setMyLocationEnabled(true);
+
+    }
+
+    private void goToLocation(double x, double y, int zoom) {
+        LatLng ll = new LatLng(x,y);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll,zoom);
+        googleMap.moveCamera(update);
+    }
 }
